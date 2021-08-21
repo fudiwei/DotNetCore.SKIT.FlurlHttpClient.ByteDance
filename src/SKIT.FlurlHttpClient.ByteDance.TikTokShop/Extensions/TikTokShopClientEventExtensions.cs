3e2d723a01464b303ce2e92798f4133f02973b6d
Fu Diwei <fudiwei@sina.com>
2021-08-21 20:43:15 +0800

    feat(tiktokshop): 新增反序列化消息推送事件的扩展方法

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientEventExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop
+{
+    /// <summary>
+    /// 为 <see cref="TikTokShopClient"/> 提供消息推送事件的扩展方法。
+    /// </summary>
+    public static class TikTokShopClientEventExtensions
+    {
+        /// <summary>
+        /// <para>从 JSON 反序列化得到 <see cref="TikTokShopEvent"/> 对象。</para>
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="client"></param>
+        /// <param name="callbackJson"></param>
+        /// <returns></returns>
+        public static TEvent DeserializeEvent<TEvent>(this TikTokShopClient client, string callbackJson)
+            where TEvent : TikTokShopEvent, new()
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            if (string.IsNullOrEmpty(callbackJson)) throw new ArgumentNullException(callbackJson);
+
+            try
+            {
+                return client.JsonSerializer.Deserialize<TEvent>(callbackJson);
+            }
+            catch (Exception ex)
+            {
+                throw new Exceptions.TikTokShopEventSerializationException("Deserialize event failed. Please see the `InnerException` for more details.", ex);
+            }
+        }
+    }
+}

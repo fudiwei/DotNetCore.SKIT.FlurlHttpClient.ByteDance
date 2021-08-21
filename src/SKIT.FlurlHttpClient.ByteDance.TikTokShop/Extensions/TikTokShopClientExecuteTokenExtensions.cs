644eeed9a4bd821207ef2be9f6c55d2b29ee0a64
Fu Diwei <fudiwei@sina.com>
2021-08-21 21:00:03 +0800

    feat(tiktokshop): 新增刷新请求凭证的接口

--- src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteTokenExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteTokenExtensions.cs
@@ -31,5 +31,26 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop
 
             return await client.SendRequestWithJsonAsync<Models.TokenCreateResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /token/refresh 接口。</para>
+        /// <para>REF: https://op.jinritemai.com/docs/guide-docs/9/21 </para>
+        /// <para>REF: https://op.jinritemai.com/docs/guide-docs/9/22 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.TokenRefreshResponse> ExecuteTokenRefreshAsync(this TikTokShopClient client, Models.TokenRefreshRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "token", "refresh")
+                .SetQueryParam("app_key", client.Credentials.AppKey);
+
+            return await client.SendRequestWithJsonAsync<Models.TokenRefreshResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
     }
 }
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/Token/TokenRefreshResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /token/refresh 接口的响应。</para>
+    /// </summary>
+    public class TokenRefreshResponse : TikTokShopResponse<TokenRefreshResponse.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : TokenCreateResponse.Types.Data
+            {
+            }
+        }
+    }
+}

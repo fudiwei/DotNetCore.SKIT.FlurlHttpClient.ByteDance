71b3623a4783a0089397ac8bfb318555ecb3edd1
Fu Diwei <fudiwei@sina.com>
2021-08-26 16:31:07 +0800

    feat(microapp): 新增数据缓存相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/ByteDanceMicroAppClientExecuteAppsStorageExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
+{
+    public static class ByteDanceMicroAppClientExecuteAppsStorageExtensions
+    {
+        private static string GenerateRequestSignature<TRequest>(this ByteDanceMicroAppClient client, TRequest request, string signMethod, string sessionKey)
+            where TRequest : ByteDanceMicroAppRequest
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (sessionKey == null) throw new ArgumentNullException(nameof(sessionKey));
+
+            /*
+             * REF: https://microapp.bytedance.com/docs/zh-CN/mini-app/develop/server/other/user-login-sign/
+             * REF: https://microapp.bytedance.com/docs/zh-CN/mini-game/develop/server/other/user-login-sign/
+             */
+
+            string json = client.JsonSerializer.Serialize(request);
+
+            switch (signMethod)
+            {
+                case "hmac_sha256":
+                    return Security.HMACSHA256Utility.Hash(sessionKey, json).ToLower();
+
+                default:
+                    return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /apps/set_user_storage 接口。</para>
+        /// <para>REF: https://microapp.bytedance.com/docs/zh-CN/mini-app/develop/server/data-caching/set-user-storage </para>
+        /// <para>REF: https://microapp.bytedance.com/docs/zh-CN/mini-game/develop/server/data-caching/set-user-storage </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AppsSetUserStorageResponse> ExecuteAppsSetUserStorageAsync(this ByteDanceMicroAppClient client, Models.AppsSetUserStorageRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (string.IsNullOrEmpty(request.Signature))
+                request.Signature = client.GenerateRequestSignature(request, request.SignMethod, request.SessionKey!);
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "apps", "set_user_storage")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("openid", request.OpenId)
+                .SetQueryParam("signature", request.Signature)
+                .SetQueryParam("sig_method", request.SignMethod);
+
+            return await client.SendRequestWithJsonAsync<Models.AppsSetUserStorageResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /apps/remove_user_storage 接口。</para>
+        /// <para>REF: https://microapp.bytedance.com/docs/zh-CN/mini-app/develop/server/data-caching/remove-user-storage </para>
+        /// <para>REF: https://microapp.bytedance.com/docs/zh-CN/mini-game/develop/server/data-caching/remove-user-storage </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AppsRemoveUserStorageResponse> ExecuteAppsRemoveUserStorageAsync(this ByteDanceMicroAppClient client, Models.AppsRemoveUserStorageRequest request, CancellationToken cancellationToken = default)        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (string.IsNullOrEmpty(request.Signature))
+                request.Signature = client.GenerateRequestSignature(request, request.SignMethod, request.SessionKey!);
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "apps", "remove_user_storage")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("openid", request.OpenId)
+                .SetQueryParam("signature", request.Signature)
+                .SetQueryParam("sig_method", request.SignMethod);
+
+            return await client.SendRequestWithJsonAsync<Models.AppsRemoveUserStorageResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsStorage/AppsSetUserStorageResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /apps/set_user_storage 接口的响应。</para>
+    /// </summary>
+    public class AppsSetUserStorageResponse : ByteDanceMicroAppResponse
+    {
+    }
+}

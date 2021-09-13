df5db829c392e0b6c8c2b58be2508eb3b9a42d2e
Fu Diwei <fudiwei@sina.com>
2021-09-14 01:14:22 +0800

    feat(tiktokshop): 新增 SPU 相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/SPU/SPUGetSPUInformationBySPUIdRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /spu/getSpuInfoBySpuId 接口的请求。</para>
+    /// </summary>
+    public class SPUGetSPUInformationBySPUIdRequest : TikTokShopRequest
+    {
+        /// <summary>
+        /// 获取或设置 SPU ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("spu_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("spu_id")]
+        public long SPUId { get; set; }
+
+        protected internal override string GetApiMethod()
+        {
+            return string.Intern("spu.getSpuInfoBySpuId");
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/SPU/SPUGetSPUTemplateRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /spu/getSpuTpl 接口的请求。</para>
+    /// </summary>
+    public class SPUGetSPUTemplateRequest : TikTokShopRequest
+    {
+        /// <summary>
+        /// 获取或设置类目 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("category_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("category_id")]
+        public int CategoryId { get; set; }
+
+        protected internal override string GetApiMethod()
+        {
+            return string.Intern("spu.getSpuTpl");
+        }
+    }
+}

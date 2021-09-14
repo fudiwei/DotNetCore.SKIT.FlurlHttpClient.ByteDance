dd34eceb0395843f969a4d8910bfebadff8a8e7b
Fu Diwei <fudiwei@sina.com>
2021-09-15 01:04:22 +0800

    feat(tiktokshop): 新增机会品相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/OpportunityProduct/OpportunityProductClueRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /opptyProduct/clue 接口的请求。</para>
+    /// </summary>
+    public class OpportunityProductClueRequest : TikTokShopRequest
+    {
+        /// <summary>
+        /// 获取或设置机会品线索类型。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("oppty_clue_type")]
+        [System.Text.Json.Serialization.JsonPropertyName("oppty_clue_type")]
+        public string OpptyClueType { get; set; } = string.Empty;
+
+        protected internal override string GetApiMethod()
+        {
+            return string.Intern("opptyProduct.clue");
+        }
+    }
+}

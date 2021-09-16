43847401f5b80749eb35deb39cd27032e9de89b2
Fu Diwei <fudiwei@sina.com>
2021-09-16 15:38:09 +0800

    feat(tiktokshop): 新增物流地址相关接口

--- src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteAddressExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteAddressExtensions.cs
@@ -70,5 +70,45 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop
 
             return await client.SendRequestWithJsonAsync<Models.AddressUpdateResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /address/getAreasByProvince 接口。</para>
+        /// <para>REF: https://op.jinritemai.com/docs/api-docs/16/334 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AddressGetAreasByProvinceResponse> ExecuteAddressGetAreasByProvinceAsync(this TikTokShopClient client, Models.AddressGetAreasByProvinceRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "address", "getAreasByProvince")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.AddressGetAreasByProvinceResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /address/getProvince 接口。</para>
+        /// <para>REF: https://op.jinritemai.com/docs/api-docs/16/336 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AddressGetProvinceResponse> ExecuteAddressGetProvinceAsync(this TikTokShopClient client, Models.AddressGetProvinceRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "address", "getProvince")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.AddressGetProvinceResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
     }
 }
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/Address/AddressGetProvinceRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /address/getProvince 接口的请求。</para>
+    /// </summary>
+    public class AddressGetProvinceRequest : TikTokShopRequest
+    {
+        protected internal override string GetApiMethod()
+        {
+            return string.Intern("address.getProvince");
+        }
+    }
+}

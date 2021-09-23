d4eea4c49641c8242534aee493e21856298da9a7
Fu Diwei <fudiwei@sina.com>
2021-09-23 15:07:12 +0800

    feat(tiktokshop): 新增跨境相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/CrossBorder/CrossBorderQueryBalanceRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /crossBorder/queryBalance 接口的请求。</para>
+    /// </summary>
+    public class CrossBorderQueryBalanceRequest : TikTokShopRequest
+    {
+        /// <summary>
+        /// 获取或设置店铺 ID。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("shop_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("shop_id")]
+        public int ShopId { get; set; }
+
+        protected internal override string GetApiMethod()
+        {
+            return string.Intern("crossBorder.queryBalance");
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Models/CrossBorder/CrossBorderWarehouseInOutboundEventResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /crossBorder/warehouseInOutboundEvent 接口的响应。</para>
+    /// </summary>
+    public class CrossBorderWarehouseInOutboundEventResponse : TikTokShopResponse<CrossBorderWarehouseInOutboundEventResponse.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : CrossBorderStockTakingResponse.Types.Data
+            {
+            }
+        }
+
+        public override bool IsSuccessful()
+        {
+            return base.IsSuccessful() && Data?.ErrorCode == 0;
+        }
+    }
+}

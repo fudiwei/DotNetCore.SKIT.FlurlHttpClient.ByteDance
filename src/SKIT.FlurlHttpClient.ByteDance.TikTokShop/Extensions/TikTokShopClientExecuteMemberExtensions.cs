23f601da79bc6064a6fa7c7306f3869ae0f329b5
Fu Diwei <fudiwei@sina.com>
2021-09-25 20:35:39 +0800

    feat(tiktokshop): 新增联盟订单相关接口

--- src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteMemberExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTokShop/Extensions/TikTokShopClientExecuteMemberExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Flurl;
@@ -29,6 +27,26 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTokShop
                 .SetQueryParam("access_token", request.AccessToken);
 
             return await client.SendRequestWithJsonAsync<Models.MemberSearchListResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /member/batchUpdate 接口。</para>
+        /// <para>REF: https://op.jinritemai.com/docs/api-docs/46/329 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.MemberBatchUpdateResponse> ExecuteMemberBatchUpdateAsync(this TikTokShopClient client, Models.MemberBatchUpdateRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "member", "batchUpdate")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.MemberBatchUpdateResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
     }
 }

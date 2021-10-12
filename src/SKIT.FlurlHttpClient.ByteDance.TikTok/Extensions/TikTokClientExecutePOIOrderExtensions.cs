39c37f00f7dc97f4eb1a8daf95c3c20faf69e90b
Fu Diwei <fudiwei@sina.com>
2021-10-12 16:54:04 +0800

    feat(tiktok): 新增生活服务开放能力端内消息同步相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecutePOIOrderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTok
+{
+    public static class TikTokClientExecutePOIOrderExtensions
+    {
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/order/sync 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6848798579239290892 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIOrderSyncResponse> ExecutePOIOrderSyncAsync(this TikTokClient client, Models.POIOrderSyncRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            if (request.DateTimeUnixMilliseconds == null)
+                request.DateTimeUnixMilliseconds = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "order", "sync")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIOrderSyncResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+    }
+}

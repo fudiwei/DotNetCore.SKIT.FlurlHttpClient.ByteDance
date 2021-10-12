88e1a9e8fc381ce30de38439c33790b1af4a52a6
Fu Diwei <fudiwei@sina.com>
2021-10-12 18:18:14 +0800

    feat(tiktok): 新增生活服务开放能力交易系统接入相关接口

--- src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecutePOIOrderExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecutePOIOrderExtensions.cs
@@ -31,5 +31,66 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTok
 
             return await client.SendRequestWithJsonAsync<Models.POIOrderSyncResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
+
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/order/status 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6848806501990074382 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIOrderStatusResponse> ExecutePOIOrderStatusAsync(this TikTokClient client, Models.POIOrderStatusRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "order", "status")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIOrderStatusResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+        
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/order/confirm 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6875919211793778699 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIOrderConfirmResponse> ExecutePOIOrderConfirmAsync(this TikTokClient client, Models.POIOrderConfirmRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "order", "confirm")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIOrderConfirmResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+
+        /// <summary>
+        /// <para>异步调用 [GET] /poi/order/bill/token 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6898241240786126861 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIOrderBillTokenResponse> ExecutePOIOrderBillTokenAsync(this TikTokClient client, Models.POIOrderBillTokenRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "poi", "order", "bill", "token")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("bill_date", request.BillDateString);
+
+            return await client.SendRequestWithJsonAsync<Models.POIOrderBillTokenResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
     }
 }

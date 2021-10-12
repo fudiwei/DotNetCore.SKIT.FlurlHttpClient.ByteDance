0d42b5df2131d49b84056db1f1f55b9a425f5147
Fu Diwei <fudiwei@sina.com>
2021-10-12 21:36:35 +0800

    feat(tiktok): 新增生活服务开放能力预售券交易接入相关接口

--- src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecutePOIExternalExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecutePOIExternalExtensions.cs
@@ -70,5 +70,67 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTok
             return await client.SendRequestWithJsonAsync<Models.POIExternalHotelOrderCancelResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
         }
         #endregion
+
+        #region PresaleGroupon
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/ext/presale_groupon/order/create 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6875919211626121220 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIExternalPresaleGrouponCreateResponse> ExecutePOIExternalPresaleGrouponCreateAsync(this TikTokClient client, Models.POIExternalPresaleGrouponCreateRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "ext", "presale_groupon", "order", "create")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIExternalPresaleGrouponCreateResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+        
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/ext/presale_groupon/order/commit 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6875919211626072068 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIExternalPresaleGrouponCommitResponse> ExecutePOIExternalPresaleGrouponCommitAsync(this TikTokClient client, Models.POIExternalPresaleGrouponCommitRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "ext", "presale_groupon", "order", "commit")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIExternalPresaleGrouponCommitResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+        
+        /// <summary>
+        /// <para>异步调用 [POST] /poi/ext/presale_groupon/order/cancel 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6875919211626022916 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.POIExternalPresaleGrouponCancelResponse> ExecutePOIExternalPresaleGrouponCancelAsync(this TikTokClient client, Models.POIExternalPresaleGrouponCancelRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "poi", "ext", "presale_groupon", "order", "cancel")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.POIExternalPresaleGrouponCancelResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+        #endregion
     }
 }
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Models/POIExternal/PresaleGroupon/POIExternalPresaleGrouponCreateResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.TikTok.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /poi/ext/presale_groupon/order/create 接口的响应。</para>
+    /// </summary>
+    public class POIExternalPresaleGrouponCreateResponse : TikTokResponse<POIExternalPresaleGrouponCreateResponse.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : TikTokResposneData
+            {
+                /// <summary>
+                /// 获取或设置外部订单 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("order_ext_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("order_ext_id")]
+                public string OrderExternalId { get; set; } = default!;
+            }
+        }
+    }
+}

a69e53232e55afde713598acde005c2d7fc879e8
Fu Diwei <fudiwei@sina.com>
2021-10-14 14:54:28 +0800

    feat(tiktok): 新增数据开放服务星图数据相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Models/Star/StarAuthorScoreV2Response.cs
@@ -0,0 +1,77 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.TikTok.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /star/author_score_v2 接口的响应。</para>
+    /// </summary>
+    public class StarAuthorScoreV2Response : TikTokResponse<StarAuthorScoreV2Response.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : TikTokResposneData
+            {
+                /// <summary>
+                /// 获取或设置抖音号。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("unique_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("unique_id")]
+                public string UniqueId { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置昵称。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("nick_name")]
+                [System.Text.Json.Serialization.JsonPropertyName("nick_name")]
+                public string Nickname { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置性价比指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("cp_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("cp_score")]
+                public double CPScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置种草指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("shop_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("shop_score")]
+                public double ShopScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置传播指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("spread_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("spread_score")]
+                public double SpreadScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置星图指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("star_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("star_score")]
+                public double StarScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置合作指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("cooperation_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("cooperation_score")]
+                public double CooperationScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置涨粉指数。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("growth_score")]
+                [System.Text.Json.Serialization.JsonPropertyName("growth_score")]
+                public double GrowthScore { get; set; }
+
+                /// <summary>
+                /// 获取或设置更新时间戳。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("update_timestamp")]
+                [System.Text.Json.Serialization.JsonPropertyName("update_timestamp")]
+                public long UpdateTimestamp { get; set; }
+            }
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Models/Star/StarHotListResponse.cs
@@ -0,0 +1,90 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.TikTok.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /star/hot_list 接口的响应。</para>
+    /// </summary>
+    public class StarHotListResponse : TikTokResponse<StarHotListResponse.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : TikTokResposneData
+            {
+                public static class Types
+                {
+                    public class Star
+                    {
+                        /// <summary>
+                        /// 获取或设置抖音号。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("unique_id")]
+                        [System.Text.Json.Serialization.JsonPropertyName("unique_id")]
+                        public string UniqueId { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置昵称。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("nick_name")]
+                        [System.Text.Json.Serialization.JsonPropertyName("nick_name")]
+                        public string Nickname { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置粉丝数。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("follower")]
+                        [System.Text.Json.Serialization.JsonPropertyName("follower")]
+                        public int FansCount { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置热榜排名。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("rank")]
+                        [System.Text.Json.Serialization.JsonPropertyName("rank")]
+                        public int Rank { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置热榜指数。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("score")]
+                        [System.Text.Json.Serialization.JsonPropertyName("score")]
+                        public double Score { get; set; }
+
+                        /// <summary>
+                        /// 获取或设置标签列表。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("tags")]
+                        [System.Text.Json.Serialization.JsonPropertyName("tags")]
+                        public string[] TagList { get; set; } = default!;
+                    }
+                }
+
+                /// <summary>
+                /// 获取或设置达人列表。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("list")]
+                [System.Text.Json.Serialization.JsonPropertyName("list")]
+                public Types.Star[] StarList { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置达人热榜类型。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("hot_list_type")]
+                [System.Text.Json.Serialization.JsonPropertyName("hot_list_type")]
+                public int HotListType { get; set; }
+
+                /// <summary>
+                /// 获取或设置达人热榜类型说明。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("hot_list_description")]
+                [System.Text.Json.Serialization.JsonPropertyName("hot_list_description")]
+                public string HotListDescription { get; set; } = default!;
+
+                /// <summary>
+                /// 获取或设置达人热榜更新时间戳。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("hot_list_update_timestamp")]
+                [System.Text.Json.Serialization.JsonPropertyName("hot_list_update_timestamp")]
+                public long UpdateTimestamp { get; set; }
+            }
+        }
+    }
+}

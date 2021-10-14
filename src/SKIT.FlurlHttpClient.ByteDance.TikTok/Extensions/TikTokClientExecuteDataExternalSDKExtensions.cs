f2d8086cd4001806f69580c258d0d68df2969860
Fu Diwei <fudiwei@sina.com>
2021-10-14 12:39:57 +0800

    feat(tiktok): 新增数据开放服务 SDK 分享视频数据相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecuteDataExternalSDKExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTok
+{
+    public static class TikTokClientExecuteDataExternalSDKExtensions
+    {
+        /// <summary>
+        /// <para>异步调用 [GET] /data/external/sdk_share 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6848798428881881092 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.DataExternalSDKShareResponse> ExecuteDataExternalSDKShareAsync(this TikTokClient client, Models.DataExternalSDKShareRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "data", "external", "sdk_share")
+                .SetQueryParam("access_token", request.AccessToken)
+                .SetQueryParam("begin_date", request.BeginDateString)
+                .SetQueryParam("end_date", request.EndDateString);
+
+            return await client.SendRequestWithJsonAsync<Models.DataExternalSDKShareResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+    }
+}

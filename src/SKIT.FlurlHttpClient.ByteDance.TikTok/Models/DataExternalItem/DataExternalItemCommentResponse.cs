c3dbbe7034b81ae866b0470b4661692917a68173
Fu Diwei <fudiwei@sina.com>
2021-10-14 13:13:02 +0800

    feat(tiktok): 新增数据开放服务视频数据相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Models/DataExternalItem/DataExternalItemCommentResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.TikTok.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /data/external/item/comment 接口的响应。</para>
+    /// </summary>
+    public class DataExternalItemCommentResponse : TikTokResponse<DataExternalItemCommentResponse.Types.Data>
+    {
+        public static class Types
+        {
+            public class Data : TikTokResposneData
+            {
+                public static class Types
+                {
+                    public class Result
+                    {
+                        /// <summary>
+                        /// 获取或设置日期字符串（格式：yyyy-MM-dd）。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("date")]
+                        [System.Text.Json.Serialization.JsonPropertyName("date")]
+                        public string DateString { get; set; } = default!;
+
+                        /// <summary>
+                        /// 获取或设置当天新评论数。
+                        /// </summary>
+                        [Newtonsoft.Json.JsonProperty("comment")]
+                        [System.Text.Json.Serialization.JsonPropertyName("comment")]
+                        public int NewCommentCount { get; set; }
+                    }
+                }
+
+                /// <summary>
+                /// 获取或设置结果列表。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("result_list")]
+                [System.Text.Json.Serialization.JsonPropertyName("result_list")]
+                public Types.Result[] ResultList { get; set; } = default!;
+            }
+        }
+    }
+}

3e04fb16bd2f3f4621932b1cbf1047089cd0ad9f
Fu Diwei <fudiwei@sina.com>
2021-10-14 15:10:25 +0800

    feat(tiktok): 新增数据开放服务抖音影视综艺榜单数据相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Models/Discovery/DiscoveryEntertainmentRankItemRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.TikTok.Models
+{
+    /// <summary>
+    /// <para>表示 [GET] /discovery/ent/rank/item 接口的请求。</para>
+    /// </summary>
+    public class DiscoveryEntertainmentRankItemRequest : TikTokRequest
+    {
+        /// <summary>
+        /// 获取或设置榜单类型。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int Type { get; set; }
+
+        /// <summary>
+        /// 获取或设置榜单版本。
+        /// </summary>
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
+        public int? Version { get; set; }
+    }
+}

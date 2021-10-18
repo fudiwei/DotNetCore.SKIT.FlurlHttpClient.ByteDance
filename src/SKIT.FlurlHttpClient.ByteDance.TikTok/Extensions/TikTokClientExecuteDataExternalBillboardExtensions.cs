e5cbca3bee624ee53116a0c1300d7ff12063efeb
Fu Diwei <fudiwei@sina.com>
2021-10-18 20:17:54 +0800

    feat(tiktok): 新增数据开放服务热门视频榜相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/Extensions/TikTokClientExecuteDataExternalBillboardExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.ByteDance.TikTok
+{
+    public static class TikTokClientExecuteDataExternalBillboardExtensions
+    {
+        #region HotVideo
+        /// <summary>
+        /// <para>异步调用 [GET] /data/extern/billboard/hot_video 接口。</para>
+        /// <para>REF: https://open.douyin.com/platform/doc/6908910551393437707 </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.DataExternalBillboardHotVideoResponse> ExecuteDataExternalBillboardHotVideoAsync(this TikTokClient client, Models.DataExternalBillboardHotVideoRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Get, "data", "extern", "billboard", "hot_video")
+                .SetQueryParam("access_token", request.AccessToken);
+
+            return await client.SendRequestWithJsonAsync<Models.DataExternalBillboardHotVideoResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+        #endregion
+    }
+}

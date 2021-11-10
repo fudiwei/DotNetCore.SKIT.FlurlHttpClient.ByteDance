8eae76cb5bf7dc69c59299e7be5b318aebdb8afc
Fu Diwei <fudiwei@sina.com>
2021-11-10 15:06:13 +0800

    test: 优化 CodeStyle 测试流程

--- test/SKIT.FlurlHttpClient.ByteDance.OceanEngine.UnitTests/TestConfigs.cs
+++ test/SKIT.FlurlHttpClient.ByteDance.OceanEngine.UnitTests/TestConfigs.cs
@@ -8,19 +8,26 @@ namespace SKIT.FlurlHttpClient.ByteDance.OceanEngine.UnitTests
     {
         static TestConfigs()
         {
-            // NOTICE: 请在项目根目录下按照 appsettings.json 的格式填入测试参数。
+            // NOTICE: 请在项目根目录下按照 appsettings.json 的格式新建 appsettings.local.json 填入测试参数。
             // WARN: 敏感信息请不要提交到 git！
 
-            using var stream = File.OpenRead("appsettings.json");
-            using var json = JsonDocument.Parse(stream);
+            try
+            {
+                using var stream = File.OpenRead("appsettings.local.json");
+                using var json = JsonDocument.Parse(stream);
 
-            var config = json.RootElement.GetProperty("OceanEngineConfig");
-            OceanEngineAppId = config.GetProperty("AppId").GetInt64();
-            OceanEngineAppSecret = config.GetProperty("AppSecret").GetString();
-            OceanEngineAccessToken = config.GetProperty("AccessToken").GetString();
+                var config = json.RootElement.GetProperty("OceanEngineConfig");
+                OceanEngineAppId = config.GetProperty("AppId").GetInt64();
+                OceanEngineAppSecret = config.GetProperty("AppSecret").GetString();
+                OceanEngineAccessToken = config.GetProperty("AccessToken").GetString();
 
-            ProjectSourceDirectory = json.RootElement.GetProperty("ProjectSourceDirectory").GetString();
-            ProjectTestDirectory = json.RootElement.GetProperty("ProjectTestDirectory").GetString();
+                ProjectSourceDirectory = json.RootElement.GetProperty("ProjectSourceDirectory").GetString();
+                ProjectTestDirectory = json.RootElement.GetProperty("ProjectTestDirectory").GetString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("加载配置文件 appsettings.local.json 失败", ex);
+            }
         }
 
         public static readonly long OceanEngineAppId;
--- test/SKIT.FlurlHttpClient.ByteDance.TikTok.UnitTests/TestConfigs.cs
+++ test/SKIT.FlurlHttpClient.ByteDance.TikTok.UnitTests/TestConfigs.cs
@@ -8,19 +8,26 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTok.UnitTests
     {
         static TestConfigs()
         {
-            // NOTICE: 请在项目根目录下按照 appsettings.json 的格式填入测试参数。
+            // NOTICE: 请在项目根目录下按照 appsettings.json 的格式新建 appsettings.local.json 填入测试参数。
             // WARN: 敏感信息请不要提交到 git！
 
-            using var stream = File.OpenRead("appsettings.json");
-            using var json = JsonDocument.Parse(stream);
+            try
+            {
+                using var stream = File.OpenRead("appsettings.local.json");
+                using var json = JsonDocument.Parse(stream);
 
-            var config = json.RootElement.GetProperty("TikTokConfig");
-            TikTokClientKey = config.GetProperty("ClientKey").GetString();
-            TikTokClientSecret = config.GetProperty("ClientSecret").GetString();
-            TikTokAccessToken = config.GetProperty("AccessToken").GetString();
+                var config = json.RootElement.GetProperty("TikTokConfig");
+                TikTokClientKey = config.GetProperty("ClientKey").GetString();
+                TikTokClientSecret = config.GetProperty("ClientSecret").GetString();
+                TikTokAccessToken = config.GetProperty("AccessToken").GetString();
 
-            ProjectSourceDirectory = json.RootElement.GetProperty("ProjectSourceDirectory").GetString();
-            ProjectTestDirectory = json.RootElement.GetProperty("ProjectTestDirectory").GetString();
+                ProjectSourceDirectory = json.RootElement.GetProperty("ProjectSourceDirectory").GetString();
+                ProjectTestDirectory = json.RootElement.GetProperty("ProjectTestDirectory").GetString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("加载配置文件 appsettings.local.json 失败", ex);
+            }
         }
 
         public static readonly string TikTokClientKey;

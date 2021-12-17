fd27e90900fba5f4a19bf125a46d69133a2d4d56
Fu Diwei <fudiwei@sina.com>
2021-12-17 20:55:09 +0800

    refactor: clean code

--- src/SKIT.FlurlHttpClient.ByteDance.TikTok/TikTokClient.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.TikTok/TikTokClient.cs
@@ -111,9 +111,10 @@ namespace SKIT.FlurlHttpClient.ByteDance.TikTok
             where T : TikTokResponse, new()
         {
             byte[] bytes = await flurlResponse.GetBytesAsync().ConfigureAwait(false);
-            bool jsonable = 
-                (bytes.Length > 1 && bytes[0] == 91 && bytes[bytes.Length - 1] == 93) || // "[...]"
-                (bytes.Length > 1 && bytes[0] == 123 && bytes[bytes.Length - 1] == 125); // "{...}"
+            bool jsonable =
+                (bytes.FirstOrDefault() == 91 && bytes.LastOrDefault() == 93) || // "[...]"
+                (bytes.FirstOrDefault() == 123 && bytes.LastOrDefault() == 125); // "{...}"
+
             T result = jsonable ? JsonSerializer.Deserialize<T>(Encoding.UTF8.GetString(bytes)) : new T();
 
             result.RawStatus = flurlResponse.StatusCode;

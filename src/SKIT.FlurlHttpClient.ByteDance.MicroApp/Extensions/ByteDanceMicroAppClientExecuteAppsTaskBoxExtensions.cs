45c0c8a5d68b4c45045eff2663d48723ccc5c4ca
Fu Diwei <fudiwei@sina.com>
2022-05-02 11:44:40 +0800

    feat(microapp): 新增小程序任务后台任务上传接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Extensions/ByteDanceMicroAppClientExecuteAppsTaskBoxExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+
+namespace SKIT.FlurlHttpClient.ByteDance.MicroApp
+{
+    public static class ByteDanceMicroAppClientExecuteAppsTaskBoxExtensions
+    {
+        /// <summary>
+        /// <para>异步调用 [POST] /apps/taskbox/add_task 接口。</para>
+        /// <para>REF: https://microapp.bytedance.com/docs/zh-CN/mini-app/develop/server/other/taskbox-task-upload </para>
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public static async Task<Models.AppsTaskBoxAddTaskResponse> ExecuteAppsTaskBoxAddTaskAsync(this ByteDanceMicroAppClient client, Models.AppsTaskBoxAddTaskRequest request, CancellationToken cancellationToken = default)
+        {
+            if (client is null) throw new ArgumentNullException(nameof(client));
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            IFlurlRequest flurlReq = client
+                .CreateRequest(request, HttpMethod.Post, "apps", "taskbox", "add_task");
+
+            return await client.SendRequestWithJsonAsync<Models.AppsTaskBoxAddTaskResponse>(flurlReq, data: request, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsTaskBox/AppsTaskBoxAddTaskResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /apps/taskbox/add_task 接口的响应。</para>
+    /// </summary>
+    public class AppsTaskBoxAddTaskResponse : ByteDanceMicroAppResponse
+    {
+        public static class Types
+        {
+            public class Data
+            {
+                /// <summary>
+                /// 获取或设置任务 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("task_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("task_id")]
+                public string TaskId { get; set; } = default!;
+            }
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("message")]
+        [System.Text.Json.Serialization.JsonPropertyName("message")]
+        public override string? ErrorMessage { get; set; }
+
+        /// <summary>
+        /// 获取或设置返回数据。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("data")]
+        [System.Text.Json.Serialization.JsonPropertyName("data")]
+        public Types.Data Data { get; set; } = default!;
+    }
+}

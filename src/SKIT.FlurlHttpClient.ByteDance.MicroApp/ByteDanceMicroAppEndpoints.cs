bfd28ced6fc217660b01fcb28b5b3322db678fd3
Fu Diwei <fudiwei@sina.com>
2022-08-25 10:48:54 +0800

    feat(microapp): 新增沙盒域名接入点

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/ByteDanceMicroAppEndpoints.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/ByteDanceMicroAppEndpoints.cs
@@ -11,6 +11,11 @@
         public const string API_MINIAPP = "https://developer.toutiao.com/api";
 
         /// <summary>
+        /// 小程序域名沙盒环境域名。
+        /// </summary>
+        public const string API_MINIAPP_SANDBOX = "https://open-sandbox.douyin.com/api";
+
+        /// <summary>
         /// 小游戏域名。
         /// </summary>
         public const string API_MINIGAME = "https://minigame.zijieapi.com/mgplatform/api";
@@ -24,5 +29,10 @@
         /// 泛知识课程接口域名。
         /// </summary>
         public const string API_PRODUCT = "https://developer-product.zijieapi.com/product/api";
+
+        /// <summary>
+        /// 泛知识课程接口沙盒环境域名。
+        /// </summary>
+        public const string API_PRODUCT_SANDBOX = "https://open-sandbox.douyin.com/product/api";
     }
 }

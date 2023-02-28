db2c884af1258a744d89d567b90ea3bb3e164734
Fu Diwei <fudiwei@sina.com>
2023-03-01 00:15:38 +0800

    fix(douyinopen): 修复生活服务开放能力门店信息查询接口响应模型定义错误

--- src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Models/GoodLife/Shop/GoodLifeShopPOIQueryV1Response.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Models/GoodLife/Shop/GoodLifeShopPOIQueryV1Response.cs
@@ -13,40 +13,105 @@ namespace SKIT.FlurlHttpClient.ByteDance.DouyinOpen.Models
                 {
                     public class POI
                     {
-                        /// <summary>
-                        /// 获取或设置门店 ID。
-                        /// </summary>
-                        [Newtonsoft.Json.JsonProperty("poi_id")]
-                        [System.Text.Json.Serialization.JsonPropertyName("poi_id")]
-                        public string POIId { get; set; } = default!;
+                        public static class Types
+                        {
+                            public class AccountInfo
+                            {
+                                public static class Types
+                                {
+                                    public class Account
+                                    {
+                                        /// <summary>
+                                        /// 获取或设置账户 ID。
+                                        /// </summary>
+                                        [Newtonsoft.Json.JsonProperty("account_id")]
+                                        [System.Text.Json.Serialization.JsonPropertyName("account_id")]
+                                        [System.Text.Json.Serialization.JsonConverter(typeof(System.Text.Json.Converters.NumericalStringReadOnlyConverter))]
+                                        public string AccountId { get; set; } = default!;
+
+                                        /// <summary>
+                                        /// 获取或设置账户昵称。
+                                        /// </summary>
+                                        [Newtonsoft.Json.JsonProperty("account_name")]
+                                        [System.Text.Json.Serialization.JsonPropertyName("account_name")]
+                                        public string AccountName { get; set; } = default!;
+
+                                        /// <summary>
+                                        /// 获取或设置账户类型。
+                                        /// </summary>
+                                        [Newtonsoft.Json.JsonProperty("account_type")]
+                                        [System.Text.Json.Serialization.JsonPropertyName("account_type")]
+                                        public string AccountType { get; set; } = default!;
+                                    }
+                                }
+
+                                /// <summary>
+                                /// 获取或设置供应商账户信息。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("parent_account")]
+                                [System.Text.Json.Serialization.JsonPropertyName("parent_account")]
+                                public Types.Account? ParentAccount { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置门店账户信息。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("poi_account")]
+                                [System.Text.Json.Serialization.JsonPropertyName("poi_account")]
+                                public Types.Account? POIAccount { get; set; }
+                            }
+
+                            public class POIInfo
+                            {
+                                /// <summary>
+                                /// 获取或设置门店 ID。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("poi_id")]
+                                [System.Text.Json.Serialization.JsonPropertyName("poi_id")]
+                                public string POIId { get; set; } = default!;
+
+                                /// <summary>
+                                /// 获取或设置门店名称。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("poi_name")]
+                                [System.Text.Json.Serialization.JsonPropertyName("poi_name")]
+                                public string POIName { get; set; } = default!;
+
+                                /// <summary>
+                                /// 获取或设置门店地址。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("address")]
+                                [System.Text.Json.Serialization.JsonPropertyName("address")]
+                                public string Address { get; set; } = default!;
+
+                                /// <summary>
+                                /// 获取或设置纬度。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("latitude")]
+                                [System.Text.Json.Serialization.JsonPropertyName("latitude")]
+                                public double Latitude { get; set; }
+
+                                /// <summary>
+                                /// 获取或设置经度。
+                                /// </summary>
+                                [Newtonsoft.Json.JsonProperty("longitude")]
+                                [System.Text.Json.Serialization.JsonPropertyName("longitude")]
+                                public double Longitude { get; set; }
+                            }
+                        }
 
                         /// <summary>
-                        /// 获取或设置门店名称。
+                        /// 获取或设置账户信息。
                         /// </summary>
-                        [Newtonsoft.Json.JsonProperty("poi_name")]
-                        [System.Text.Json.Serialization.JsonPropertyName("poi_name")]
-                        public string POIName { get; set; } = default!;
+                        [Newtonsoft.Json.JsonProperty("account")]
+                        [System.Text.Json.Serialization.JsonPropertyName("account")]
+                        public Types.AccountInfo AccountInfo { get; set; } = default!;
 
                         /// <summary>
-                        /// 获取或设置门店地址。
+                        /// 获取或设置门店信息。
                         /// </summary>
-                        [Newtonsoft.Json.JsonProperty("address")]
-                        [System.Text.Json.Serialization.JsonPropertyName("address")]
-                        public string Address { get; set; } = default!;
-
-                        /// <summary>
-                        /// 获取或设置纬度。
-                        /// </summary>
-                        [Newtonsoft.Json.JsonProperty("latitude")]
-                        [System.Text.Json.Serialization.JsonPropertyName("latitude")]
-                        public double Latitude { get; set; }
-
-                        /// <summary>
-                        /// 获取或设置经度。
-                        /// </summary>
-                        [Newtonsoft.Json.JsonProperty("longitude")]
-                        [System.Text.Json.Serialization.JsonPropertyName("longitude")]
-                        public double Longitude { get; set; }
+                        [Newtonsoft.Json.JsonProperty("poi")]
+                        [System.Text.Json.Serialization.JsonPropertyName("poi")]
+                        public Types.POIInfo POIInfo { get; set; } = default!;
                     }
                 }
 

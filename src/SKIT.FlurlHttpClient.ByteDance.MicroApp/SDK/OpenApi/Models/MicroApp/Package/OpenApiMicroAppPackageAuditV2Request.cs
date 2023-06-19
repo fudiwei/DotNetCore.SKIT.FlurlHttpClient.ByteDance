67e1fe2b56cfe51ac9ede86b3ed62a718e28dcde
RHQYZ <fudiwei@sina.com>
2023-06-19 17:37:45 +0800

    Update OpenApiMicroAppPackageAuditV2Request.cs

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Models/MicroApp/Package/OpenApiMicroAppPackageAuditV2Request.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Models/MicroApp/Package/OpenApiMicroAppPackageAuditV2Request.cs
@@ -8,21 +8,21 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.SDK.OpenApi.Models
     public class OpenApiMicroAppPackageAuditV2Request : ByteDanceMicroAppOpenApiRequest
     {
         /// <summary>
-        /// 获取或设置本次需要审核的宿主端英文简称列表。
+        /// 获取或设置本次提审的宿主端列表。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("hostNames")]
         [System.Text.Json.Serialization.JsonPropertyName("hostNames")]
         public IList<string> HostNames { get; set; } = new List<string>();
 
         /// <summary>
-        /// 本次提审的备注信息。
+        /// 获取或设置本次提审的备注信息。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("auditNote")]
         [System.Text.Json.Serialization.JsonPropertyName("auditNote")]
         public string? AuditNote { get; set; }
 
         /// <summary>
-        /// 是否用上次失败的版本重新提审，不传或传0表示正常提审，传1表示用上次审核失败的版本重新提审。
+        /// 获取或设置重新提审方式。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("auditWay")]
         [System.Text.Json.Serialization.JsonPropertyName("auditWay")]

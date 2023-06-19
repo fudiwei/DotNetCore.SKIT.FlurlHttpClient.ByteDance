322f0dcc223584ca654dcaba84ad6528348db742
RHQYZ <fudiwei@sina.com>
2023-06-19 17:52:51 +0800

    Update ComponentAuthorizedEvent.cs

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Events/Auth/ComponentAuthorizedEvent.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Events/Auth/ComponentAuthorizedEvent.cs
@@ -27,39 +27,39 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.SDK.OpenApi.Events
         /// </summary>
         [Newtonsoft.Json.JsonProperty("AppName")]
         [System.Text.Json.Serialization.JsonPropertyName("AppName")]
-        [System.Xml.Serialization.XmlElement("AppName")]
-        public string AppName { get; set; } = default!;
+        [System.Xml.Serialization.XmlElement("AppName", IsNullable = true)]
+        public string? AppName { get; set; }
 
         /// <summary>
-        /// 获取或设置代创建的小程序图标。
+        /// 获取或设置代创建的小程序图标路径。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("AppIcon")]
         [System.Text.Json.Serialization.JsonPropertyName("AppIcon")]
-        [System.Xml.Serialization.XmlElement("AppIcon")]
-        public string AppIcon { get; set; } = default!;
+        [System.Xml.Serialization.XmlElement("AppIcon", IsNullable = true)]
+        public string? AppIconPath { get; set; }
 
         /// <summary>
         /// 获取或设置代创建的小程序公司主体名称。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("CompanyName")]
         [System.Text.Json.Serialization.JsonPropertyName("CompanyName")]
-        [System.Xml.Serialization.XmlElement("CompanyName")]
-        public string CompanyName { get; set; } = default!;
+        [System.Xml.Serialization.XmlElement("CompanyName", IsNullable = true)]
+        public string? CompanyName { get; set; }
 
         /// <summary>
         /// 获取或设置代创建的小程序超管掩码邮箱。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("AppSuperAdminEmail")]
         [System.Text.Json.Serialization.JsonPropertyName("AppSuperAdminEmail")]
-        [System.Xml.Serialization.XmlElement("AppSuperAdminEmail")]
-        public string SuperAdminEmail { get; set; } = default!;
+        [System.Xml.Serialization.XmlElement("AppSuperAdminEmail", IsNullable = true)]
+        public string? SuperAdminEmail { get; set; }
 
         /// <summary>
         /// 获取或设置代创建的小程序超管掩码手机号。
         /// </summary>
         [Newtonsoft.Json.JsonProperty("AppSuperAdminMobile")]
         [System.Text.Json.Serialization.JsonPropertyName("AppSuperAdminMobile")]
-        [System.Xml.Serialization.XmlElement("AppSuperAdminMobile")]
-        public string SuperAdminMobileNumber { get; set; } = default!;
+        [System.Xml.Serialization.XmlElement("AppSuperAdminMobile", IsNullable = true)]
+        public string? SuperAdminMobileNumber { get; set; }
     }
 }

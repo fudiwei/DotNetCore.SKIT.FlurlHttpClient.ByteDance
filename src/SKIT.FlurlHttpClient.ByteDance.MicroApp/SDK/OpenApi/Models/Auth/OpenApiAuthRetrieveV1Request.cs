8db0a8df06adc30b4e6001efcece8f499b708ae2
RHQYZ <fudiwei@sina.com>
2023-06-19 17:35:38 +0800

    Update OpenApiAuthRetrieveV1Request.cs

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Models/Auth/OpenApiAuthRetrieveV1Request.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/SDK/OpenApi/Models/Auth/OpenApiAuthRetrieveV1Request.cs
@@ -6,21 +6,21 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.SDK.OpenApi.Models
     public class OpenApiAuthRetrieveV1Request : ByteDanceMicroAppOpenApiRequest
     {
         /// <summary>
-        /// 第三方小程序应用 appid
+        /// <inheritdoc/>
         /// </summary>
         [Newtonsoft.Json.JsonProperty("component_appid")]
         [System.Text.Json.Serialization.JsonPropertyName("component_appid")]
         public override string? ComponentAppId { get; set; }
 
         /// <summary>
-        /// 第三方小程序应用接口调用凭据
+        /// <inheritdoc/>
         /// </summary>
         [Newtonsoft.Json.JsonProperty("component_access_token")]
         [System.Text.Json.Serialization.JsonPropertyName("component_access_token")]
         public override string? ComponentAccessToken { get; set; }
 
         /// <summary>
-        /// 授权小程序 appid
+        /// <inheritdoc/>
         /// </summary>
         [Newtonsoft.Json.JsonProperty("authorization_appid")]
         [System.Text.Json.Serialization.JsonPropertyName("authorization_appid")]

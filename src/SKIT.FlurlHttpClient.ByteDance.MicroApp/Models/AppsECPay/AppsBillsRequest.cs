0a93ad504c19a27404b349def51512d49229e285
xiedi <763171683@qq.com>
2023-09-01 15:26:50 +0800

    Revert "fix #20 invalid sign"
    This reverts commit 2c636c127cdc9b5e84dbf659e8e5d16f2a62f47f.

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsECPay/AppsBillsRequest.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsECPay/AppsBillsRequest.cs
@@ -8,43 +8,43 @@ namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.Models
         /// <summary>
         /// 获取或设置小程序第三方平台应用 ID。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("thirdparty_id")]
-        [System.Text.Json.Serialization.JsonPropertyName("thirdparty_id")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string? ComponentAppId { get; set; }
 
         /// <summary>
         /// 获取或设置小程序 AppId。如果不指定将使用构造 <see cref="ByteDanceMicroAppClient"/> 时的 <see cref="ByteDanceMicroAppClientOptions.AppId"/> 参数。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("app_id")]
-        [System.Text.Json.Serialization.JsonPropertyName("app_id")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string? AppId { get; set; }
 
         /// <summary>
         /// 获取或设置商户号。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("merchant_id")]
-        [System.Text.Json.Serialization.JsonPropertyName("merchant_id")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string MerchantId { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置账单日期字符串（格式：yyyyMMdd）。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("bill_date")]
-        [System.Text.Json.Serialization.JsonPropertyName("bill_date")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string DateString { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置账单类型。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("bill_type")]
-        [System.Text.Json.Serialization.JsonPropertyName("bill_type")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string BillType { get; set; } = string.Empty;
 
         /// <summary>
         /// 获取或设置签名。如果不指定将由系统自动生成。
         /// </summary>
-        [Newtonsoft.Json.JsonProperty("sign")]
-        [System.Text.Json.Serialization.JsonPropertyName("sign")]
+        [Newtonsoft.Json.JsonIgnore]
+        [System.Text.Json.Serialization.JsonIgnore]
         public string? Signature { get; set; }
     }
 }

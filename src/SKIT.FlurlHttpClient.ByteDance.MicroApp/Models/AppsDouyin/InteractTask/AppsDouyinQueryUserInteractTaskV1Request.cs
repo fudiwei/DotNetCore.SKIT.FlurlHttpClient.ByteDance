502fbed3a92253f93cc871707bfbe88a15fd6394
Fu Diwei <fudiwei@sina.com>
2024-03-07 16:36:17 +0800

    feat(microapp): 新增分享任务相关接口

---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsDouyin/InteractTask/AppsDouyinQueryUserInteractTaskV1Request.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /apps/v1/douyin/query_user_interact_task/ 接口的请求。</para>
+    /// </summary>
+    public class AppsDouyinQueryUserInteractTaskV1Request : DouyinMicroAppRequest
+    {
+        /// <summary>
+        /// 获取或设置小程序 AppId。如果不指定将使用构造 <see cref="DouyinMicroAppClient"/> 时的 <see cref="DouyinMicroAppClientOptions.AppId"/> 参数。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("app_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("app_id")]
+        public string? AppId { get; set; }
+
+        /// <summary>
+        /// 获取或设置开启任务的用户 OpenId。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("open_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("open_id")]
+        public string OpenId { get; set; } = string.Empty;
+
+        /// <summary>
+        /// 获取或设置任务 ID 列表。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("task_id")]
+        [System.Text.Json.Serialization.JsonPropertyName("task_id")]
+        public IList<string> TaskIdList { get; set; } = new List<string>();
+    }
+}
---
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/Models/AppsShare/Task/AppsShareCreateTaskV1Response.cs
@@ -0,0 +1,28 @@
+namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.Models
+{
+    /// <summary>
+    /// <para>表示 [POST] /apps/v1/share/create_task/ 接口的响应。</para>
+    /// </summary>
+    public class AppsShareCreateTaskV1Response : DouyinMicroAppResponse
+    {
+        public static class Types
+        {
+            public class Data
+            {
+                /// <summary>
+                /// 获取或设置任务 ID。
+                /// </summary>
+                [Newtonsoft.Json.JsonProperty("task_id")]
+                [System.Text.Json.Serialization.JsonPropertyName("task_id")]
+                public string TaskId { get; set; } = default!;
+            }
+        }
+
+        /// <summary>
+        /// 获取或设置返回数据。
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty("data")]
+        [System.Text.Json.Serialization.JsonPropertyName("data")]
+        public Types.Data Data { get; set; } = default!;
+    }
+}

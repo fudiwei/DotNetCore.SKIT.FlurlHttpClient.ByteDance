963c882bb66710f74922a19252380b55f1ae61b9
Fu Diwei <fudiwei@sina.com>
2024-03-12 14:15:17 +0800

    feat(douyinopen): 新增获取 OpenTicket 接口

--- src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Extensions/DouyinOpenClientExecuteOpenExtensions.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Extensions/DouyinOpenClientExecuteOpenExtensions.cs
@@ -2,34 +2,33 @@ using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Flurl;
 using Flurl.Http;
 
 namespace SKIT.FlurlHttpClient.ByteDance.DouyinOpen
 {
-    public static class DouyinOpenClientExecuteJSExtensions
+    public static class DouyinOpenClientExecuteOpenExtensions
     {
         /// <summary>
-        /// <para>异步调用 [GET] /js/getticket 接口。</para>
+        /// <para>异步调用 [GET] /open/getticket 接口。</para>
         /// <para>
         /// REF: <br/>
-        /// <![CDATA[ https://developer.open-douyin.com/docs/resource/zh-CN/dop/develop/openapi/tools-ability/jsb-management/get-jsb-ticket ]]>
+        /// <![CDATA[ https://developer.open-douyin.com/docs/resource/zh-CN/dop/develop/openapi/tools-ability/jsb-management/open-ticket ]]>
         /// </para>
         /// </summary>
         /// <param name="client"></param>
         /// <param name="request"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public static async Task<Models.JSGetTicketResponse> ExecuteJSGetTicketAsync(this DouyinOpenClient client, Models.JSGetTicketRequest request, CancellationToken cancellationToken = default)
+        public static async Task<Models.OpenGetTicketResponse> ExecuteOpenGetTicketAsync(this DouyinOpenClient client, Models.OpenGetTicketRequest request, CancellationToken cancellationToken = default)
         {
             if (client is null) throw new ArgumentNullException(nameof(client));
             if (request is null) throw new ArgumentNullException(nameof(request));
 
             IFlurlRequest flurlReq = client
                 .CreateFlurlRequest(request, HttpMethod.Get, "js", "getticket")
-                .WithHeader("content-type", request.AccessToken);
+                .WithHeader("access-token", request.AccessToken);
 
-            return await client.SendFlurlRequestAsJsonAsync<Models.JSGetTicketResponse>(flurlReq, data: request, cancellationToken: cancellationToken).ConfigureAwait(false);
+            return await client.SendFlurlRequestAsJsonAsync<Models.OpenGetTicketResponse>(flurlReq, data: request, cancellationToken: cancellationToken).ConfigureAwait(false);
         }
     }
 }
--- src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Models/Open/OpenGetTicketResponse.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.DouyinOpen/Models/Open/OpenGetTicketResponse.cs
@@ -1,9 +1,9 @@
 namespace SKIT.FlurlHttpClient.ByteDance.DouyinOpen.Models
 {
     /// <summary>
-    /// <para>表示 [GET] /js/getticket 接口的响应。</para>
+    /// <para>表示 [GET] /open/getticket 接口的响应。</para>
     /// </summary>
-    public class JSGetTicketResponse : DouyinOpenResponse<JSGetTicketResponse.Types.Data>
+    public class OpenGetTicketResponse : DouyinOpenResponse<OpenGetTicketResponse.Types.Data>
     {
         public static class Types
         {

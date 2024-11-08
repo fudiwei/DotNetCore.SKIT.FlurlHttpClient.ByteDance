6a04985d5983784792180f9e9aaa44b1a064ea25
Fu Diwei <fudiwei@sina.com>
2024-11-08 12:08:37 +0800

    feat(microapp): bump version to v3.7.0

--- src/SKIT.FlurlHttpClient.ByteDance.MicroApp/ExtendedSDK/Webcast/Models/GamingCon/WebcastGamingConRoundCoGameUploadUserDataRequest.cs
+++ src/SKIT.FlurlHttpClient.ByteDance.MicroApp/ExtendedSDK/Webcast/Models/GamingCon/WebcastGamingConRoundCoGameUploadUserDataRequest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace SKIT.FlurlHttpClient.ByteDance.MicroApp.ExtendedSDK.Webcast.Models
 {
     /// <summary>
-    /// <para>表示 [POST] /gaming_con/round/co_game_upload_user_data​ 接口的请求。</para>
+    /// <para>表示 [POST] /gaming_con/round/co_game_upload_user_data 接口的请求。</para>
     /// </summary>
     public class WebcastGamingConRoundCoGameUploadUserDataRequest : DouyinMicroAppWebcastRequest
     {
